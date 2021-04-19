1eb3a115689a9833d092ae687ea1ec88d45007f7
VenkatManikanta <venkat.purushothapu@gmail.com>
2021-04-19 22:50:08 +0530

    First Commit

---
+++ LinkListDemo/src/mainnn.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class mainnn {
+
+	public static void main(String[] args) {
+		
+	   Set<String> hashSet = new HashSet<String>();
+	  
+	  hashSet.add("JAVA");
+	  hashSet.add("J2EE");
+	  hashSet.add("JSP");
+	  hashSet.add("JAVA");
+	   hashSet.add("SERVLETS");
+	   hashSet.add("JSP");
+		  System.out.println(hashSet);
+		 
+	}
+
+}
